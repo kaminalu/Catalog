897173f6ced103da4ece53786a681fb8d07da747
Amy Kamin <akamin@hawk.iit.edu>
2016-09-27 18:05:27 -0500

    refreshed items controller with set category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:index, :new]
 
   # GET /items
   # GET /items.json
@@ -65,6 +66,9 @@ class ItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
+    end
+    def set_category
+      @category = Category.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
