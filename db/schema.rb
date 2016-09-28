f55fde22e9b47f0bcefcf8ef2b2d1efdc908c0ac
Amy Kamin <akamin@hawk.iit.edu>
2016-09-27 19:44:01 -0500

    added category columnn to items
    ran:  $ rails g migration AddCategoryToItems category:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927225517) do
+ActiveRecord::Schema.define(version: 20160928004047) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20160927225517) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
 end
