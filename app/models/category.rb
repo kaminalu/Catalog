366b6c0299d10133d9996d61584122ff6aa65dac
Amy Kamin <akamin@hawk.iit.edu>
2016-09-27 19:53:03 -0500

    fixed syntax error

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :category, presence: true
   validates_length_of :category, :within => 1..20
-  has_many :items, , inverse_of: :category, dependent: :destroy
+  has_many :items, inverse_of: :category, dependent: :destroy
   accepts_nested_attributes_for :items
 end
