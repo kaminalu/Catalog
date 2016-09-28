4fb0e949f3cbf4e9ff121a33df8f87c30fccf9e2
Amy Kamin <akamin@hawk.iit.edu>
2016-09-28 16:16:52 -0500

    lists all items under a category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
   validates :category, presence: true
   validates_length_of :category, :within => 1..20
-  has_many :items, inverse_of: :category, dependent: :destroy
-  accepts_nested_attributes_for :items
+  has_many :items, dependent: :destroy
 end
